38b30e8a62eb213e805614f6a5768f37d4561f00
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2022-01-07 17:23:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["images.pexels.com", "github.com"],
+    domains: [
+      "images.pexels.com",
+      "github.com",
+      "png.pngitem.com",
+      "images.unsplash.com",
+    ],
   },
   loader: "imgix",
   path: "",
